74e38c01a2242584c88fbad564ba8b6f1a372624
Yuriy Mikhailov <apofiz2036@bk.ru>
2025-02-14 17:20:08 +0500

    Add model for images

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
 
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type_image')
+
+
 admin.site.register(Place, PlaceAdmin)
+admin.site.register(Image, ImageAdmin)
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -8,6 +8,33 @@ class Place(models.Model):
     lng = models.FloatField(verbose_name='Долгота')
     lan = models.FloatField(verbose_name='Широта')
 
+    first_image = models.ForeignKey(
+        'Image',
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='places_first',
+        verbose_name='Первая картинка')
+
+    second_image = models.ManyToManyField(
+        'Image',
+        blank=True,
+        related_name='places_second',
+        verbose_name='Вторая картинка'
+    )
+
     def __str__(self):
         return self.title
-    
+
+
+class Image(models.Model):
+    TYPE_IMAGE = [
+        ('first_image', 'Первая картинка'),
+        ('second_image', 'Вторая картинка')
+    ]
+
+    title = models.CharField(max_length=100, verbose_name='Название картинки')
+    type_image = models.CharField(max_length=50, choices=TYPE_IMAGE, verbose_name='тип картинки')
+    image = models.ImageField(verbose_name='Картинка')
+
+    def __str__(self):
+        return self.title
