44a5549bdd41b1b6571450b43f95f7b3467a5dd2
Yuriy Mikhailov <apofiz2036@bk.ru>
2025-03-27 07:22:02 +0500

    изменил STATIC_ROOT

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -16,7 +16,7 @@ if DEBUG:
     MEDIA_ROOT = BASE_DIR / 'media'
 else:
     STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 
